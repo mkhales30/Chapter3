1045c6439410afc3d6feb1f3ae1a309369d29135
Khales Rahman <106184751+mkhales30@users.noreply.github.com>
2022-10-06 23:17:20 -0400

    Create Chcicken.java

---
+++ Chcicken.java
@@ -0,0 +1,27 @@
+public class Chicken extends FarmAnimal {
+
+private String sound;
+
+public Chicken(String name, String gender, double weight, int age, String sound) {
+super(name, gender, weight, age);
+this.sound = sound;
+}
+
+public String getSound() {
+return sound;
+}
+
+public void setSound(String sound) {
+this.sound = sound;
+}
+
+@Override
+public String feedLoadingSchedule() {
+return getName() + ": 8am-4pm";
+}
+
+@Override
+public String toString() {
+return "Chicken " + getSound() + super.toString();
+}
+}
